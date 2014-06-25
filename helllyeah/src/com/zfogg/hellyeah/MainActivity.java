b07d3f8877a8870022475b517bba283551372af7
app_a4eb6e81 <app_a4eb6e81@ip-10-235-45-61.ec2.internal>
2014-06-25 07:36:33 +0000

    AndroidKickstartR generation

---
+++ helllyeah/src/com/zfogg/hellyeah/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.zfogg.hellyeah;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity
+    extends SherlockActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
